1169cfc393965e6ff04fec2d17cce6418bd3213b
hdewit <hdewit@ilionx.com>
2019-12-17 16:32:39 +0100

    schalen naar 4 formaten

--- Thumbnail/index.js
+++ Thumbnail/index.js
@@ -16,7 +16,10 @@ const ONE_MEGABYTE = 1024 * 1024;
 const ONE_MINUTE = 60 * 1000;
 const uploadOptions = { bufferSize: 4 * ONE_MEGABYTE, maxBuffers: 20 };
 
-const containerName = process.env.BLOB_CONTAINER_NAME;
+const containerNameLarge = process.env.BLOB_CONTAINER_NAME_LARGE;
+const containerNameMedium = process.env.BLOB_CONTAINER_NAME_MEDIUM;
+const containerNameSmall = process.env.BLOB_CONTAINER_NAME_SMALL;
+const containerNameThumb = process.env.BLOB_CONTAINER_NAME_THUMB;
 const accountName = process.env.AZURE_STORAGE_ACCOUNT_NAME;
 const accessKey = process.env.AZURE_STORAGE_ACCOUNT_ACCESS_KEY;
 
@@ -36,38 +39,54 @@ module.exports = (context, eventGridEvent, inputBlob) => {
   const contentType = context.bindingData.data.contentType;
   const blobUrl = context.bindingData.data.url;
   const blobName = blobUrl.slice(blobUrl.lastIndexOf("/")+1);
+  const widthInPixelsFormatLarge = 1200;
+  const widthInPixelsFormatMedium = 600;
+  const widthInPixelsFormatSmall = 300;
+  const widthInPixelsFormatThumb = 100;
 
   Jimp.read(inputBlob).then( (thumbnail) => {
+    uploadResizedCopy(thumbnail, context, widthInPixelsFormatLarge, containerNameLarge, context);
+    uploadResizedCopy(thumbnail, context, widthInPixelsFormatMedium, containerNameMedium, context);
+    uploadResizedCopy(thumbnail, context, widthInPixelsFormatSmall, containerNameSmall, context);
+    uploadResizedCopy(thumbnail, context, widthInPixelsFormatThumb, containerNameThumb, context);
+  });
+};
 
-    thumbnail.resize(widthInPixels, Jimp.AUTO);
+function uploadResizedCopy(thumbnail, widthInPixels, containerName, context) {
 
-    const options = {
-      contentSettings: { contentType: contentType }
-    };
+  const aborter = Aborter.timeout(30 * ONE_MINUTE);
+  const contentType = context.bindingData.data.contentType;
+  const blobUrl = context.bindingData.data.url;
+  const blobName = blobUrl.slice(blobUrl.lastIndexOf("/")+1);
 
-    thumbnail.getBuffer(Jimp.MIME_PNG, async (err, buffer) => {
+  thumbnail.resize(widthInPixels, Jimp.AUTO);
 
-      const readStream = stream.PassThrough();
-      readStream.end(buffer);
+  const options = {
+    contentSettings: { contentType: contentType }
+  };
 
-      const containerURL = ContainerURL.fromServiceURL(serviceURL, containerName);
-      const blockBlobURL = BlockBlobURL.fromContainerURL(containerURL, blobName);
+  thumbnail.getBuffer(Jimp.MIME_PNG, async (err, buffer) => {
 
-      try {   
+    const readStream = stream.PassThrough();
+    readStream.end(buffer);
 
-        await uploadStreamToBlockBlob(aborter, readStream,
+    const containerURL = ContainerURL.fromServiceURL(serviceURL, containerName);
+    const blockBlobURL = BlockBlobURL.fromContainerURL(containerURL, blobName);
+
+    try {
+
+      await uploadStreamToBlockBlob(aborter, readStream,
           blockBlobURL, uploadOptions.bufferSize, uploadOptions.maxBuffers,
           { blobHTTPHeaders: { blobContentType: "image/jpeg" } });
 
-      } catch (err) {
+    } catch (err) {
 
-        context.log(err.message);
+      context.log(err.message);
 
-      } finally {        
+    } finally {
 
-        context.done();
+      context.done();
 
-      }
-    });
+    }
   });
-};
+}
