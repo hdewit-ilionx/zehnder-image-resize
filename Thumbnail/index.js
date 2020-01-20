57fcb32b72242a96a9e89fec90fa73dcce1ad3d9
hdewit <hdewit@ilionx.com>
2020-01-20 13:46:28 +0100

    schalen naar 4 formaten

--- Thumbnail/index.js
+++ Thumbnail/index.js
@@ -1,5 +1,6 @@
 const stream = require('stream');
 const Jimp = require('jimp');
+const storage = require('azure-storage');
 
 const {
   Aborter,
@@ -12,54 +13,70 @@ const {
   uploadStreamToBlockBlob
 } = require("@azure/storage-blob");
 
+const connectionString = "DefaultEndpointsProtocol=https;AccountName=zehndertestblob;AccountKey=Ehvx4kfmCzA7rR76woABA1WLG/rcOLcnnC/jObMY1edem2SrKze1d9b1ZyIQBZDfY9M8Z9AWj7Qy393rKKRulw==;EndpointSuffix=core.windows.net"
 const ONE_MEGABYTE = 1024 * 1024;
 const ONE_MINUTE = 60 * 1000;
-const uploadOptions = { bufferSize: 4 * ONE_MEGABYTE, maxBuffers: 20 };
+const uploadOptions = { bufferSize: 4 * ONE_MEGABYTE, maxBuffers:  20 };
+const aborter = Aborter.timeout(30 * ONE_MINUTE);
 
+const accountName = process.env.AZURE_STORAGE_ACCOUNT_NAME;
+const accessKey = process.env.AZURE_STORAGE_ACCOUNT_ACCESS_KEY;
+const containerNameUpload = process.env.BLOB_CONTAINER_NAME;
+const containerNameFullsize = process.env.BLOB_CONTAINER_NAME_FULLSIZE;
 const containerNameLarge = process.env.BLOB_CONTAINER_NAME_LARGE;
 const containerNameMedium = process.env.BLOB_CONTAINER_NAME_MEDIUM;
 const containerNameSmall = process.env.BLOB_CONTAINER_NAME_SMALL;
 const containerNameThumb = process.env.BLOB_CONTAINER_NAME_THUMB;
-const accountName = process.env.AZURE_STORAGE_ACCOUNT_NAME;
-const accessKey = process.env.AZURE_STORAGE_ACCOUNT_ACCESS_KEY;
+let widthInPixelsFormatLarge = parseInt(process.env.IMAGE_WIDTH_LARGE);
+let widthInPixelsFormatMedium = parseInt(process.env.IMAGE_WIDTH_MEDIUM);
+let widthInPixelsFormatSmall = parseInt(process.env.IMAGE_WIDTH_SMALL);
+let widthInPixelsFormatThumb = parseInt(process.env.IMAGE_WIDTH_THUMB);
+
+/**
+const widthInPixelsFormatLarge = 1200;
+const widthInPixelsFormatMedium = 600;
+const widthInPixelsFormatSmall = 300;
+const widthInPixelsFormatThumb = 100;
+**/
 
 const sharedKeyCredential = new SharedKeyCredential(
-  accountName,
-  accessKey);
+    accountName,
+    accessKey);
 const pipeline = StorageURL.newPipeline(sharedKeyCredential);
 const serviceURL = new ServiceURL(
-  `https://${accountName}.blob.core.windows.net`,
-  pipeline
+    `https://${accountName}.blob.core.windows.net`,
+    pipeline
 );
 
-module.exports = (context, eventGridEvent, inputBlob) => {  
+module.exports = (context, eventGridEvent, inputBlob) => {
 
-  const aborter = Aborter.timeout(30 * ONE_MINUTE);
-  const widthInPixels = 100;
-  const contentType = context.bindingData.data.contentType;
   const blobUrl = context.bindingData.data.url;
-  const blobName = blobUrl.slice(blobUrl.lastIndexOf("/")+1);
-  const widthInPixelsFormatLarge = 1200;
-  const widthInPixelsFormatMedium = 600;
-  const widthInPixelsFormatSmall = 300;
-  const widthInPixelsFormatThumb = 100;
-
-  Jimp.read(inputBlob).then( (thumbnail) => {
-    uploadResizedCopy(thumbnail, context, widthInPixelsFormatLarge, containerNameLarge, context);
-    uploadResizedCopy(thumbnail, context, widthInPixelsFormatMedium, containerNameMedium, context);
-    uploadResizedCopy(thumbnail, context, widthInPixelsFormatSmall, containerNameSmall, context);
-    uploadResizedCopy(thumbnail, context, widthInPixelsFormatThumb, containerNameThumb, context);
-  });
+  context.log("Blob uploaded to " + blobUrl);
+  context.log("Container with uploaded images = " + containerNameUpload);
+  if (blobUrl.indexOf("/" + containerNameUpload + "/") > -1) {
+      Jimp.read(inputBlob).then( (thumbnail) => {
+        uploadResizedCopy(thumbnail, context, 0, containerNameFullsize, false);
+        uploadResizedCopy(thumbnail, context, widthInPixelsFormatLarge, containerNameLarge, true);
+        uploadResizedCopy(thumbnail, context, widthInPixelsFormatMedium, containerNameMedium, true);
+        uploadResizedCopy(thumbnail, context, widthInPixelsFormatSmall, containerNameSmall, true);
+        uploadResizedCopy(thumbnail, context, widthInPixelsFormatThumb, containerNameThumb, true);
+      });
+  } else {
+    context.done();
+  }
 };
 
-function uploadResizedCopy(thumbnail, widthInPixels, containerName, context) {
+function uploadResizedCopy(thumbnail, context, widthInPixels, containerName, doResize) {
 
-  const aborter = Aborter.timeout(30 * ONE_MINUTE);
   const contentType = context.bindingData.data.contentType;
   const blobUrl = context.bindingData.data.url;
   const blobName = blobUrl.slice(blobUrl.lastIndexOf("/")+1);
+  const fileExtension = blobUrl.slice(blobUrl.lastIndexOf("."));
+  const fileName = getArticleCode(containerName, blobName) + fileExtension;
 
-  thumbnail.resize(widthInPixels, Jimp.AUTO);
+  context.log("upload resized " + fileName + " to container " + containerName);
+
+  if (doResize) { thumbnail.resize(widthInPixels, Jimp.AUTO) };
 
   const options = {
     contentSettings: { contentType: contentType }
@@ -74,19 +91,24 @@ function uploadResizedCopy(thumbnail, widthInPixels, containerName, context) {
     const blockBlobURL = BlockBlobURL.fromContainerURL(containerURL, blobName);
 
     try {
-
       await uploadStreamToBlockBlob(aborter, readStream,
           blockBlobURL, uploadOptions.bufferSize, uploadOptions.maxBuffers,
           { blobHTTPHeaders: { blobContentType: "image/jpeg" } });
-
     } catch (err) {
-
       context.log(err.message);
-
     } finally {
-
       context.done();
-
     }
   });
+
+  function getArticleCode(containerName, blobName) {
+    let blobService = storage.createBlobService(connectionString);
+
+    blobService.getBlobMetadata(containerName, blobName, function (error, result) {
+      if (error) return callback(error);
+
+      return result.metadata.article_code;
+
+    });
+  }
 }
