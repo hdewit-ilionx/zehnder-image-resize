06b1d8e86712a31aed7f37dbcd40ba935024e0a5
hdewit <hdewit@ilionx.com>
2020-01-21 12:00:43 +0100

    schalen naar 4 formaten

--- Thumbnail/index.js
+++ Thumbnail/index.js
@@ -1,22 +1,20 @@
 const stream = require('stream');
 const Jimp = require('jimp');
-const storage = require('azure-storage');
 
 const {
-  Aborter,
-  BlobURL,
-  BlockBlobURL,
-  ContainerURL,
-  ServiceURL,
-  SharedKeyCredential,
-  StorageURL,
-  uploadStreamToBlockBlob
+    Aborter,
+    BlobURL,
+    BlockBlobURL,
+    ContainerURL,
+    ServiceURL,
+    SharedKeyCredential,
+    StorageURL,
+    uploadStreamToBlockBlob
 } = require("@azure/storage-blob");
 
-const connectionString = "todo"
 const ONE_MEGABYTE = 1024 * 1024;
 const ONE_MINUTE = 60 * 1000;
-const uploadOptions = { bufferSize: 4 * ONE_MEGABYTE, maxBuffers:  20 };
+const uploadOptions = { bufferSize: 5 * ONE_MEGABYTE, maxBuffers:  20 };
 const aborter = Aborter.timeout(30 * ONE_MINUTE);
 
 const accountName = process.env.AZURE_STORAGE_ACCOUNT_NAME;
@@ -27,17 +25,18 @@ const containerNameLarge = process.env.BLOB_CONTAINER_NAME_LARGE;
 const containerNameMedium = process.env.BLOB_CONTAINER_NAME_MEDIUM;
 const containerNameSmall = process.env.BLOB_CONTAINER_NAME_SMALL;
 const containerNameThumb = process.env.BLOB_CONTAINER_NAME_THUMB;
+
+/**
 let widthInPixelsFormatLarge = parseInt(process.env.IMAGE_WIDTH_LARGE);
 let widthInPixelsFormatMedium = parseInt(process.env.IMAGE_WIDTH_MEDIUM);
 let widthInPixelsFormatSmall = parseInt(process.env.IMAGE_WIDTH_SMALL);
 let widthInPixelsFormatThumb = parseInt(process.env.IMAGE_WIDTH_THUMB);
+ **/
 
-/**
 const widthInPixelsFormatLarge = 1200;
 const widthInPixelsFormatMedium = 600;
 const widthInPixelsFormatSmall = 300;
 const widthInPixelsFormatThumb = 100;
-**/
 
 const sharedKeyCredential = new SharedKeyCredential(
     accountName,
@@ -50,65 +49,54 @@ const serviceURL = new ServiceURL(
 
 module.exports = (context, eventGridEvent, inputBlob) => {
 
-  const blobUrl = context.bindingData.data.url;
-  context.log("Blob uploaded to " + blobUrl);
-  context.log("Container with uploaded images = " + containerNameUpload);
-  if (blobUrl.indexOf("/" + containerNameUpload + "/") > -1) {
-      Jimp.read(inputBlob).then( (thumbnail) => {
-        uploadResizedCopy(thumbnail, context, 0, containerNameFullsize, false);
-        uploadResizedCopy(thumbnail, context, widthInPixelsFormatLarge, containerNameLarge, true);
-        uploadResizedCopy(thumbnail, context, widthInPixelsFormatMedium, containerNameMedium, true);
-        uploadResizedCopy(thumbnail, context, widthInPixelsFormatSmall, containerNameSmall, true);
-        uploadResizedCopy(thumbnail, context, widthInPixelsFormatThumb, containerNameThumb, true);
-      });
-  } else {
-    context.done();
-  }
+    const blobUrl = context.bindingData.data.url;
+    const blobName = blobUrl.slice(blobUrl.lastIndexOf("/")+1);
+    context.log("Image " + blobName + " uploaded to " + containerNameUpload);
+    if (blobUrl.indexOf("/" + containerNameUpload + "/") > -1) {
+        Jimp.read(inputBlob).then( (thumbnail) => {
+            uploadResizedCopy(thumbnail, context, 0, containerNameFullsize, false);
+            uploadResizedCopy(thumbnail, context, widthInPixelsFormatLarge, containerNameLarge, true);
+            uploadResizedCopy(thumbnail, context, widthInPixelsFormatMedium, containerNameMedium, true);
+            uploadResizedCopy(thumbnail, context, widthInPixelsFormatSmall, containerNameSmall, true);
+            uploadResizedCopy(thumbnail, context, widthInPixelsFormatThumb, containerNameThumb, true);
+        });
+    } else {
+        context.done();
+    }
 };
 
 function uploadResizedCopy(thumbnail, context, widthInPixels, containerName, doResize) {
 
-  const contentType = context.bindingData.data.contentType;
-  const blobUrl = context.bindingData.data.url;
-  const blobName = blobUrl.slice(blobUrl.lastIndexOf("/")+1);
-  const fileExtension = blobUrl.slice(blobUrl.lastIndexOf("."));
-  const fileName = getArticleCode(containerName, blobName) + fileExtension;
+    const contentType = context.bindingData.data.contentType;
+    const blobUrl = context.bindingData.data.url;
+    const blobName = blobUrl.slice(blobUrl.lastIndexOf("/")+1);
 
-  context.log("upload resized " + fileName + " to container " + containerName);
+    if (doResize) {
+        thumbnail.resize(widthInPixels, Jimp.AUTO);
+        context.log("Image " + blobName + " resized to width " + widthInPixels);
+    };
+    context.log("Upload " + fileName + " to container " + containerName);
 
-  if (doResize) { thumbnail.resize(widthInPixels, Jimp.AUTO) };
+    const options = {
+        contentSettings: { contentType: contentType }
+    };
 
-  const options = {
-    contentSettings: { contentType: contentType }
-  };
+    thumbnail.getBuffer(Jimp.MIME_PNG, async (err, buffer) => {
 
-  thumbnail.getBuffer(Jimp.MIME_PNG, async (err, buffer) => {
+        const readStream = stream.PassThrough();
+        readStream.end(buffer);
 
-    const readStream = stream.PassThrough();
-    readStream.end(buffer);
+        const containerURL = ContainerURL.fromServiceURL(serviceURL, containerName);
+        const blockBlobURL = BlockBlobURL.fromContainerURL(containerURL, blobName);
 
-    const containerURL = ContainerURL.fromServiceURL(serviceURL, containerName);
-    const blockBlobURL = BlockBlobURL.fromContainerURL(containerURL, blobName);
-
-    try {
-      await uploadStreamToBlockBlob(aborter, readStream,
-          blockBlobURL, uploadOptions.bufferSize, uploadOptions.maxBuffers,
-          { blobHTTPHeaders: { blobContentType: "image/jpeg" } });
-    } catch (err) {
-      context.log(err.message);
-    } finally {
-      context.done();
-    }
-  });
-
-  function getArticleCode(containerName, blobName) {
-    let blobService = storage.createBlobService(connectionString);
-
-    blobService.getBlobMetadata(containerName, blobName, function (error, result) {
-      if (error) return callback(error);
-
-      return result.metadata.article_code;
-
+        try {
+            await uploadStreamToBlockBlob(aborter, readStream,
+                blockBlobURL, uploadOptions.bufferSize, uploadOptions.maxBuffers,
+                { blobHTTPHeaders: { blobContentType: "image/jpeg" } });
+        } catch (err) {
+            context.log(err.message);
+        } finally {
+            context.done();
+        }
     });
-  }
 }
